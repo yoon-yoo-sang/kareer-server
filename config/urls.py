09fa97374cf0a65a1889de3c09478a29b381f761
yoon-yoo-sang <yyss6188@naver.com>
2025-03-18 01:57:14 +0900

    add ping url

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
+from authentication.views import PingView
+
 urlpatterns = [
+    # ping
+    path("ping", PingView.as_view(), name="ping"),
     # Authentication
     path("auth/", include(("authentication.urls", "auth"), namespace="auth")),
     # Users
